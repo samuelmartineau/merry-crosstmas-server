d80a1da7321c77f08780473bba208ebc8c34b7c5
Samuel Martineau <samuel.martineau@axa.fr>
2015-10-03 14:40:35 +0200

    static server

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({
 	extended: true
 })); // support encoded bodies
+app.use('/', express.static('views'));
 
 var corsOptions = {
 	origin: 'samuelmartineau.github.io',
